8db39273988289e8b9f264209f165346cb457553
unknown <naaannnaaa412@gmail.com>
2024-10-24 09:36:08 +0300

    First_commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,71 @@
-const profile = document.querySelector('.button-profile')
-const overlay = document.querySelector('.overlay')
-const form = document.querySelector('.header-section__form')
+const profile = document.querySelectorAll('.button-profile');
+const profileCrm = document.querySelectorAll('.button-profile-crm');
+const profileWare = document.querySelectorAll('.button-profile-ware');
+const overlay = document.querySelectorAll('.overlay');
+const modalRegistration = document.querySelector('.header-section__form');
+const closeBtn = document.querySelectorAll('.form-close');
+const modalLogin = document.querySelector('.header-section__form-login');
+const accBtn = document.querySelector('.button-acc');
+const modalCrm = document.querySelector('.header-section__form-crm');
+const modalWare = document.querySelector('.header-section__form-ware');
 
-profile.addEventListener('click', function (event) {
-    overlay.style.display = 'flex'
-    form.style.display = 'flex'
+profile.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        overlay.forEach(function (o) {
+            o.style.display = 'flex';
+        });
+        modalRegistration.style.display = 'flex';
+    });
 });
 
-const closeBtn = document.querySelector('.form-close')
-
-closeBtn.addEventListener('click', function (event) {
-    overlay.style.display = 'none'
-    form.style.display = 'none'
+profileCrm.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        overlay.forEach(function (o) {
+            o.style.display = 'flex';
+        });
+        modalCrm.style.display = 'flex';
+    });
 });
 
-const formBtn = document.querySelector('.button-form')
-const inputStyle = document.querySelector('.input-style')
+profileWare.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        overlay.forEach(function (o) {
+            o.style.display = 'flex';
+        });
+        modalWare.style.display = 'flex';
+    });
+});
 
-// ::-webkit-input-placeholder
-// color: #ab3d3d
+closeBtn.forEach(function (button) {
+    button.addEventListener('click', function (event) {
+        overlay.forEach(function (o) {
+            o.style.display = 'none';
+        });
+        modalRegistration.style.display = 'none';
+        modalLogin.style.display = 'none';
+    });
+});
+
+if (accBtn) {
+    accBtn.addEventListener('click', function (event) {
+        event.preventDefault();
+        modalRegistration.style.display = 'none';
+        overlay.forEach(function (o) {
+            o.style.display = 'flex';
+        });
+        modalLogin.style.display = 'flex';
+    });
+}
+
+function toggleAccordion(element) {
+    const content = element.nextElementSibling;
+    const icon = element.querySelector('.acc-app');
+
+    if (content.style.display === "block") {
+        content.style.display = "none";
+        icon.src = "icons/down.svg";
+    } else {
+        content.style.display = "block";
+        icon.src = "icons/app.svg";
+    }
+}
